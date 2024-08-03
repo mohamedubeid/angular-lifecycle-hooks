370e6e72a49fa07fcb6ca2cfc2d44842619f4d15
MohamedUbeid9 <mubaid@vast.ps>
2024-08-03 10:42:43 +0300

    ngOnInit Hook

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,26 +1,21 @@
 import { Component, 
+  ElementRef, 
   Input, 
   OnChanges, 
-  SimpleChanges, 
   OnInit, 
+  SimpleChanges,
   ViewChild, 
-  ElementRef, 
-  DoCheck, 
-  AfterContentInit, 
-  ContentChild, 
-  AfterContentChecked,
-  AfterViewInit, 
-  AfterViewChecked,
-  OnDestroy} from '@angular/core';
+} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges {
+export class DemoComponent implements OnChanges, OnInit {
   title: string = 'Demo Component';
   @Input() message: string | undefined;
+  @ViewChild('temp') tempPara: ElementRef | undefined;
 
 
   constructor(){
@@ -29,8 +24,13 @@ export class DemoComponent implements OnChanges {
     console.log(this.message);
   }
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges) {  //it runs every time property or something in the component changed
     console.log('Demo component ngOnChanges Hook called');
     console.log(changes);
+  }
+
+  ngOnInit() {                          //it runs Once after ngOnchanges run
+    console.log('Demo component ngOnInit Hook called');
+    console.log(this.tempPara?.nativeElement.innerHTML);
   }
 }
