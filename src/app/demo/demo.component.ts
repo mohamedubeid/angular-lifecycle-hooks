a0b79d9481977d27fa976582771d9348fd2e28bf
Mohamed Ubeid <mubaid@vast.ps>
2024-08-03 15:33:45 +0300

    ngAfterContentInit Hook

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,6 @@
-import { Component, 
+import { AfterContentInit, Component, 
+  ContentChild, 
+  DoCheck, 
   ElementRef, 
   Input, 
   OnChanges, 
@@ -12,10 +14,11 @@ import { Component,
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges, OnInit {
+export class DemoComponent implements OnChanges, OnInit, DoCheck, AfterContentInit {
   title: string = 'Demo Component';
   @Input() message: string | undefined;
   @ViewChild('temp') tempPara: ElementRef | undefined;
+  @ContentChild('temp') paraContent: ElementRef | undefined;
 
 
   constructor(){
@@ -29,12 +32,18 @@ export class DemoComponent implements OnChanges, OnInit {
     console.log(changes);
   }
 
-  ngOnInit() {                          //it runs Once after ngOnchanges run
+  ngOnInit() {                          //it runs --Once-- after ngOnchanges run
     console.log('Demo component ngOnInit Hook called');
-    console.log(this.tempPara?.nativeElement.innerHTML);
+    console.log('In ngOnInit', this.tempPara?.nativeElement);
   }
 
   ngDoCheck() {                         //it runs during every change detection cycle even if there is no change in input bound properties. it runs after ngOnChanges and ngOnInit.
     console.log('Demo component ngDoCheck Hook called');
+    console.log('In ngDoCheck', this.paraContent?.nativeElement);
+  }
+
+  ngAfterContentInit() {                //it runs --Once-- after the components projected content has been fully initialized.
+    console.log('Demo component ngAfterContentInit Hook called');
+    console.log('In ngAfterContentInit', this.paraContent?.nativeElement);
   }
 }
