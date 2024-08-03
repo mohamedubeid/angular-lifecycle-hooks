58b6e510460529f398d6def9784ba2b74857e0ba
Mohamed Ubeid <mubaid@vast.ps>
2024-08-03 17:03:07 +0300

    ngOnDestroy Hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-lifecycle-hook';
   inputVal: string = '';
+  toDestroy: boolean = false;
 
   constructor(){
     console.log('App Component constructor called!');
   }
 
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit Hook of app component called');
+  }
+
+  ngAfterViewChecked() {                  //it runs --during every change detection cycle-- after the components veiw template and all its child components view templates are fully initialized.       @ViewChild and @ViewChildren          component only hook
+    console.log('ngAfterViewChecked Hook of app component called');
+
+  }
+
   onBtnCilcked(inputEl: HTMLInputElement) {
     // this.inputVal = inputEl.value;
+  }
+
+  DestroyComponent() {
+    this.toDestroy = !this.toDestroy;
   }
 }
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,9 +1,10 @@
-import { AfterContentChecked, AfterContentInit, Component, 
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, 
   ContentChild, 
   DoCheck, 
   ElementRef, 
   Input, 
   OnChanges, 
+  OnDestroy, 
   OnInit, 
   SimpleChanges,
   ViewChild, 
@@ -14,11 +15,11 @@ import { AfterContentChecked, AfterContentInit, Component,
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked {
+export class DemoComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   title: string = 'Demo Component';
   @Input() message: string | undefined;
-  @ViewChild('temp') tempPara: ElementRef | undefined;
   @ContentChild('temp') paraContent: ElementRef | undefined;
+  @ViewChild('temp') tempPara: ElementRef | undefined;
 
 
   constructor(){
@@ -34,22 +35,41 @@ export class DemoComponent implements OnChanges, OnInit, DoCheck, AfterContentIn
 
   ngOnInit() {                          //it runs --Once-- after ngOnchanges run
     console.log('Demo component ngOnInit Hook called');
-    console.log('In ngOnInit', this.tempPara?.nativeElement);
+    console.log('In ngOnInit', this.paraContent?.nativeElement, ' @ContentChild');
+    console.log('In ngOnInit', this.tempPara?.nativeElement, '  @ViewChild');
   }
 
   ngDoCheck() {                         //it runs --during every change detection cycle even if there is no change in input bound properties.-- it runs after ngOnChanges and ngOnInit.
     console.log('Demo component ngDoCheck Hook called');
-    console.log('In ngDoCheck', this.paraContent?.nativeElement);
+    console.log('In ngDoCheck', this.paraContent?.nativeElement, ' @ContentChild');
+    console.log('In ngDoCheck', this.tempPara?.nativeElement, '  @ViewChild');
   }
 
-  ngAfterContentInit() {                //it runs --Once-- after the components projected content has been fully initialized.
+  ngAfterContentInit() {                //it runs --Once-- after the components projected content has been fully initialized.             component only hook
     console.log('Demo component ngAfterContentInit Hook called');
-    console.log('In ngAfterContentInit', this.paraContent?.nativeElement);
+    console.log('In ngAfterContentInit', this.paraContent?.nativeElement, '@ContentChild');
+    console.log('In ngAfterContentInit', this.tempPara?.nativeElement, '  @ViewChild');
   }
 
-  ngAfterContentChecked() {             //it runs --during every change detection cycle even if there is no change in input bound properties.-- after the components projected content has been fully initialized.
+  ngAfterContentChecked() {             //it runs --during every change detection cycle even if there is no change in input bound properties.-- after the components projected content has been fully initialized.     @ContentChild and @ContentChildren   component only hook
     console.log('Demo component ngAfterContentChecked Hook called');
-    console.log('In ngAfterContentChecked', this.paraContent?.nativeElement.innerHTML);
+    console.log('In ngAfterContentChecked', this.paraContent?.nativeElement, '@ContentChild');
+    console.log('In ngAfterContentChecked', this.tempPara?.nativeElement.innerHTML, '  @ViewChild');
+  }
 
+  ngAfterViewInit() {                  //it runs --Once-- after the components veiw template and all its child components view templates are fully initialized.       @ViewChild and @ViewChildren          component only hook
+    console.log('Demo component ngAfterViewInit Hook called');
+    console.log('In ngAfterViewInit', this.paraContent?.nativeElement, '@ContentChild');
+    console.log('In ngAfterViewInit', this.tempPara?.nativeElement.innerHTML, '  @ViewChild');
+  }
+
+  ngAfterViewChecked() {                  //it runs --during every change detection cycle-- after the components veiw template and all its child components view templates are fully initialized.       @ViewChild and @ViewChildren          component only hook
+    console.log('Demo component ngAfterViewChecked Hook called');
+    console.log('In ngAfterViewChecked', this.paraContent?.nativeElement, '@ContentChild');
+    console.log('In ngAfterViewChecked', this.tempPara?.nativeElement.innerHTML, '  @ViewChild');
+  }
+
+  ngOnDestroy() {
+    console.log('Demo component ngOnDestroy Hook called***************************************');
   }
 }
